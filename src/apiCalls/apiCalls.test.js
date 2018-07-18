1531d08355492a1ee854860bbe1d4415da39118a
Daniela Carey <danielafcarey@gmail.com>
2018-07-18 17:45:16 -0600

    Stub tasks and courses apiCalls tests

---
+++ src/apiCalls/apiCalls.test.js
@@ -0,0 +1,143 @@
+import * as apiCalls from './apiCalls.js';
+
+describe('getAllTasks', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns an array of all tasks', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+describe('getTask', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns a task object', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+describe('addTask', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns a new task object with task id', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+describe('updateTask', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns the updated task', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+describe('deleteTask', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns the deleted task', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+describe('getAllCourses', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns an array of all courses', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+describe('getCourse', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns a course object', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+describe('addCourse', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns a new course object with course id', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+describe('updateCourse', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns the updated course', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+describe('deleteCourse', () => {
+  it('calls fetch with the correct arguments', () => {
+
+  })
+
+  it('returns the deleted course', async () => {
+
+  })
+
+  it('throws an error if the status is not ok', () => {
+
+  })
+})
+
+
