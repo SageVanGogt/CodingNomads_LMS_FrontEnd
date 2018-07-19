1b372a66a96d27a9cb335b672f675ef5285b5d49
Daniela Carey <danielafcarey@gmail.com>
2018-07-18 18:08:11 -0600

    Add post and patch task tests

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -7,6 +7,7 @@ describe('getAllTasks', () => {
       json: () => Promise.resolve(['task1', 'task2', 'task3'])
     }))
   })
+
   it('calls fetch with the correct arguments', () => {
     const url = '/api/v1/tasks';
 
@@ -27,7 +28,7 @@ describe('getAllTasks', () => {
       status: 500
     }))
 
-    const expected = 'Error: There was a problem with the fetch request.';
+    const expected = Error('There was a problem with the fetch request.');
     const result = await apiCalls.getAllTasks();
 
     expect(result).rejects.toEqual(expected);
@@ -35,36 +36,108 @@ describe('getAllTasks', () => {
 })
 
 describe('getTask', () => {
-  it('calls fetch with the correct arguments', () => {
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve({ id: 1, name: 'this is a task' })
+    }))
+  })
 
+  it('calls fetch with the correct arguments', () => {
+    const url = 'api/v1/tasks/1';
+
+    apiCalls.getTask(1);
+
+    expect(window.fetch).toHaveBeenCalledWith(url);
   })
 
   it('returns a task object', async () => {
+    const expected = { id: 1, name: 'this is a task' };
+    const result = await apiCalls.getTask(1);
 
+    expect(result).toEqual(expected);
   })
 
   it('throws an error if the status is not ok', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 500
+    }))
 
+    const expected = Error('There was a problem with the fetch request.');
+    const result = await apiCalls.getTask(1);
+
+    expect(result).rejects.toEqual(expected);
   })
 })
 
 describe('addTask', () => {
-  it('calls fetch with the correct arguments', () => {
+  let newTask;
 
+  beforeEach(() => {
+    newTask = { name: 'this is a new task' };
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve({ id: 3, name: 'this is a new task' })
+    }))
+  })
+
+  it('calls fetch with the correct arguments', () => {
+    const url = '/api/v1/tasks'
+    const options = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newTask)
+    }
+
+    apiCalls.addTask(newTask);
+
+    expect(window.fetch).toHaveBeenCalledWith(url, option);
   })
 
   it('returns a new task object with task id', async () => {
+    const expected = { id: 3, name: 'this is a new task' };
+    const result = await apiCalls.addTask(newTask);
 
+    expect(result).toEqual(expected);
   })
 
   it('throws an error if the status is not ok', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 500
+    }))
+
+    const expected = Error('There was a problem with the fetch request.');
+    const result = await apiCalls.addTask(newTask);
+
+    expect(result).toEqual(expected);
 
   })
 })
 
 describe('updateTask', () => {
-  it('calls fetch with the correct arguments', () => {
+  let updatedTask;
 
+  beforeEach(() => {
+    updatedTask = {
+      name: 'this is an updated task'
+    }
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve({ id: 3, name: 'this is an updated task' })
+    }))
+  })
+
+  it('calls fetch with the correct arguments', () => {
+    const url = '/api/v1/task/3';
+    const options = {
+      method: 'PATCH',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(updatedTask)
+    };
+
+    apiCalls.updateTask(updatedTask);
+
+    expect(window.fetch).toHaveBeenCalledWith(url, options);
   })
 
   it('returns the updated task', async () => {
