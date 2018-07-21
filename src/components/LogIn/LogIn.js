cf9f13985383ea184707ea3312529ca5b51c98e7
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-21 10:28:17 -0600

    Add conditional rendering for which type of user is logging in

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-import { connect} from 'react-redux';
+import { connect } from 'react-redux';
 import './Login.css';
 
 class LogIn extends Component {
@@ -7,14 +7,52 @@ class LogIn extends Component {
     super(props);
 
     this.state = {
-
+      userbase: 'student',
+      username: '',
+      password: ''
     };
   }
 
+  setUserbase = (userbase) => {
+    this.setState({userbase});
+  }
+
   render() {
+    const tabs = this.state.userbase === 'student' ? 
+      <div className='LogIn_tabs'>
+        <div 
+          className='LogIn_tab LogIn_tab__active' 
+          onClick={() => this.setUserbase('student')}
+        >
+          <span>Student</span>
+        </div>
+        <div 
+          className='LogIn_tab' 
+          onClick={() => this.setUserbase('admin')}
+        >
+          <span>Admin</span>
+        </div>
+      </div>
+      :
+      <div className='LogIn_tabs'>
+        <div 
+          className='LogIn_tab' 
+          onClick={() => this.setUserbase('student')}
+        >
+          <span>Student</span>
+        </div>
+        <div 
+          className='LogIn_tab LogIn_tab__active'
+          onClick={() => this.setUserbase('admin')}
+        >
+          <span>Admin</span>
+        </div>
+      </div>;
+
     return (
       <div className='LogIn'>
-        TEST
+        {tabs}
+        <form action=""></form>
       </div>
     );
   }
