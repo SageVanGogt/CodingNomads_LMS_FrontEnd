37bd7786a150ef6e6e8652957befa0355587cc79
Sage Vogt <thurmanvogt@gmail.com>
2018-07-21 13:29:03 -0600

    Move nav to the app home page
    Removed from AdminHome

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,6 +10,21 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+          <nav className="nav__site">
+            <article className="user" aria-label="user profile">
+              <img src="" alt="user profile image"/>
+              <p aria-label="user profile name">Coconut Head</p>
+            </article>
+            <hr/>
+            <h3>Main</h3>
+            <ul aria-label="main navigation list">
+              <li><a href="#"> Profile</a></li>
+              <li><a href="#"> Students</a></li>
+              <li><a href="#"> Courses</a></li>
+              <li><a href="#"> Tasks</a></li>
+              <li><a href="#"> Calendar</a></li>
+            </ul>
+          </nav>
           <Route 
             exact path={routes.HOME}
             component={() => <p>REACT YO FACE!</p>}
