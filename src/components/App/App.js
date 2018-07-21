dd0a3f23b63ab69446988c1598905801378b5138
Sage Vogt <thurmanvogt@gmail.com>
2018-07-21 14:53:50 -0600

    Add Tasks and TaskCard basic css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as routes from '../../constants/routes';
 import LogIn from '../../components/LogIn/LogIn';
 import AdminHome from '../AdminHome/AdminHome';
+import Tasks from '../Tasks/Tasks';
 import './App.css';
 
 class App extends Component {
--- src/components/TaskCard/TaskCard.js
+++ src/components/TaskCard/TaskCard.js
@@ -1,12 +1,33 @@
 
 import React, { Component} from 'react';
 import { connect } from 'react-redux';
-import './Login.css';
+import './TaskCard.css';
+import PropTypes from 'prop-types';
 
-export const TaskCard = (props) => {
-  return (
-    <div className="Task_card">
-    
-    </div>
-  );
+export class TaskCard extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const { id, name, description } = this.props;
+
+    return (
+      <div className="Task_card">
+        <h3 className="Task_name">{name}</h3>
+        <hr/>
+        <p className="Task_desc">
+          {description}
+        </p>
+      </div>
+    );
+  }
+}
+
+TaskCard.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  description: PropTypes.string
 };
+
+export default TaskCard;
