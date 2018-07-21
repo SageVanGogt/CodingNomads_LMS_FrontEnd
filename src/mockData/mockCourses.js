06bba3514773df68659855c8011300388e5508a8
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-21 13:53:20 -0600

    Create mock data for courses

---
+++ src/mockData/mockCourses.js
@@ -0,0 +1,17 @@
+export const mockCourses = [
+  {
+    id: 1,
+    name: 'Java',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent viverra nibh lacinia, sodales ipsum id, efficitur quam. Cras et felis tincidunt, ornare erat in, accumsan erat. Nulla facilisi.'
+  },
+  {
+    id: 2,
+    name: 'Javascript',
+    description: 'Lorem ipsum variable semicolon, consectetur adipiscing elit.Praesent viverra nibh lacinia, sodales ipsum id, tincidunt, ornare erat in, accumsan erat.'
+  },
+  {
+    id: 3,
+    name: 'Python',
+    description: 'elit. Praesent viverra nibh lacinia, sodales ipsum id, efficitur quam. Cras et felis tincidunt, ornare erat in, accumsan erat. Nulla facilisi.  '
+  }
+];
