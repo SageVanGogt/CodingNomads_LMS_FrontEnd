4649a7db26d26d96755329eed83501ace29b9c45
Sage Vogt <thurmanvogt@gmail.com>
2018-07-21 18:46:26 -0600

    Add boilerplate taskCreate component

---
+++ src/components/TaskCreate/TaskCreate.js
@@ -0,0 +1,19 @@
+
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import './TaskCreate.css';
+import PropTypes from 'prop-types';
+
+export class TaskCreate extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="TaskCreate-page">
+        
+      </div>
+    )
+  }
+}
---
+++ src/components/TaskCreate/TaskCreate.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { TaskCreate } from './TaskCreate';
+
+describe('TaskCreate', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<TaskCreate />);
+  })
+
+  it('should match the snapshot', () => {
+    
+  })
+})
