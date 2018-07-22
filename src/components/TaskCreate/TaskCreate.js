252e3a3b18fb91ab4c8e3dd3f051a28ed46d57dd
Sage Vogt <thurmanvogt@gmail.com>
2018-07-21 18:57:25 -0600

    Add handleChange functionality with tests

--- src/components/TaskCreate/TaskCreate.js
+++ src/components/TaskCreate/TaskCreate.js
@@ -7,13 +7,50 @@ import PropTypes from 'prop-types';
 export class TaskCreate extends Component {
   constructor() {
     super();
+    this.state = {
+      topic: '',
+      videoLink: '',
+      description: ''
+    };
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
     return (
-      <div className="TaskCreate-page">
-        
+      <div className="TaskCreate_page">
+        <form action="submit" className="TaskCreate_form">
+          <input 
+            type="text" 
+            placeholder="topic" 
+            name="topic"
+            onChange={this.handleChange}
+            value={this.state.topic}
+          />
+          <input 
+            type="text" 
+            placeholder="description" 
+            name="description"
+            onChange={this.handleChange}
+            value={this.state.description}
+          />
+          <input 
+            type="text" 
+            placeholder="url" 
+            name="videoLink"
+            onChange={this.handleChange}
+            value={this.state.videoLink}
+          />
+          <input type="submit"/>
+        </form>
       </div>
-    )
+    );
   }
 }
--- src/components/TaskCreate/TaskCreate.test.js
+++ src/components/TaskCreate/TaskCreate.test.js
@@ -10,6 +10,23 @@ describe('TaskCreate', () => {
   })
 
   it('should match the snapshot', () => {
-    
+
+  })
+
+  describe('handleChange', () => {
+    it('should update the appropriate state', () => {
+      let mockEvent = {
+        preventDefault: jest.fn(),
+        target: {
+          name: 'videoLink',
+          value: 'caden@youtube'
+        }
+      };
+      let expected = 'caden@youtube';
+      wrapper.instance().handleChange(mockEvent);
+      let actual = wrapper.state('videoLink');
+
+      expect(actual).toEqual(expected);
+    })
   })
 })
