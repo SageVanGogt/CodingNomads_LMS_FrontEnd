3752ad09e9e9a2de9e0ab60bb3ea9d6233977555
Daniela Carey <danielafcarey@gmail.com>
2018-07-24 14:30:53 -0600

    Add defaultState test to signinReducer

--- src/reducers/signinReducer.test.js
+++ src/reducers/signinReducer.test.js
@@ -12,4 +12,18 @@ describe('signinReducer', () => {
     
     expect(actual).toEqual(expected);
   })
+
+  it('returns default state if action type does not match', () => {
+    let mockAction = {
+      type: 'I_AM_NOT_AN_ACTION',
+      user: { id: 1, role: 1 }
+    };
+    
+    let defaultState = {};
+    let actual = signinReducer(defaultState, mockAction);
+    
+    expect(actual).toEqual(defaultState);
+
+  })
+
 })
