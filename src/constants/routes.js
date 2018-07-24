2e9e807b709ca48d759b2754fba1e2eeb1a617e0
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-24 08:35:21 -0600

    Create COURSE_EDIT route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,3 +4,5 @@ export const ADMIN_HOME = '/home/admin';
 export const COURSES = '/courses';
 export const TASKS = '/tasks';
 export const TASKS_EDIT = '/tasks/edit';
+export const TASK_VIEW = '/tasks';
+export const COURSE_EDIT = '/edit-course';
