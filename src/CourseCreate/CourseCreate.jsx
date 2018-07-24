2b8b485d912690a3eb0464ec4fe7ca712d3864a0
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-24 08:34:25 -0600

    Create CourseCreate boilerplate following Sage's TaskCreate page

---
+++ src/CourseCreate/CourseCreate.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { mockTasks } from '../mockData/mockTasks';
+import './TaskCreate.css';
+import PropTypes from 'prop-types';
+import * as apiCalls from '../apiCalls/apiCalls';
+
+export class CourseCreate extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      description: '',
+      tasks: [],
+      students: [],
+      tasksToDelete: [],
+      labsToDelete: []
+    };
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  fetchTasks = () => {
+    // const tasks = await apiCalls.getAllTasks();
+    const formattedTasks = this.formatTaskOptions(mockTasks);
+    return formattedTasks;
+  }
+
+  formatTaskOptions = (tasks) => {
+    //should selecting a doc activate a function sending it to state?
+    //how should we store multiple doc choices?
+    const allTaskOptions = tasks.map((task, index) => {
+      return (
+        <option
+          key={`task-${index}`}
+          name="task"
+          onClick={() => this.handleTaskSelect(task)}>
+          {task.name}
+        </option>
+      );
+    });
+    return allTaskOptions;
+  }
+
+  handleTaskSelect = task => {
+    if (!this.state.tasks.includes(task)) {
+      this.setState({
+        tasks: [...this.state.tasks, task]
+      });
+    }
+  }
+
+  render() {
+    const tasks = this.fetchTasks();
+
+    return (
+      <div className="CourseCreate_page">
+        <form action="submit" className="CourseCreate_form">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+          <input
+            type="text"
+            placeholder="description"
+            name="description"
+            onChange={this.handleChange}
+            value={this.state.description}
+          />
+          <select name="" id="">
+            {tasks}
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CourseCreate;
