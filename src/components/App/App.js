afdff80c3011602ec2c54472f0c7ec3b192bb4e0
Daniela Carey <danielafcarey@gmail.com>
2018-07-23 22:52:53 -0600

    Remove withRouter from App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as routes from '../../constants/routes';
 import LogIn from '../LogIn/LogIn';
 import AdminHome from '../AdminHome/AdminHome';
@@ -62,4 +62,4 @@ class App extends Component {
   } 
 }
 
-export default withRouter(App);
+export default App;
