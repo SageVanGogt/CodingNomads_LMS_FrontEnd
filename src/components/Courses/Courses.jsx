600334bbf9076dc978d7ec7e9802849d4cfcb96c
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-25 14:33:39 -0600

    Now courses are populated from the API!
    Had to use a cors-anywhere workaround for the fetch, and am not using an actual declared function from APIcalls file. Next to implement redux currentCourse action.

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -6,10 +6,27 @@ import CourseCard from '../CourseCard/CourseCard';
 export class Courses extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      courses: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://cors-anywhere.herokuapp.com/54.191.130.113:8080/api/admin/v1/courses', {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(response => response.json())
+      .then(results => {
+        console.log(1)
+        this.setState({courses: results.data});
+        // results.data.map(course => <CourseCard {...course} key={'course'+ course.id}/>);
+      })
   }
 
   render() {
-    const courses = mockCourses.map(course => <CourseCard {...course} key={'course' + course.id}/>);
+    const courses = this.state.courses.map(course => <CourseCard {...course} key={'course' + course.id}/>);
 
     return ( 
       <div className='courses_container'>
