2cfec43690e765ac72d92dad3b345c9ddd698685
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-25 15:58:13 -0600

    Add addCourse function to Courses page
    This creates a new, blank course and adds it to the redux store under CurrentCourse, then pushes to CourseEdit page.

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { mockCourses } from '../../mockData/mockCourses';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import './Courses.css';
 import CourseCard from '../CourseCard/CourseCard';
+import { updateCurrentCourse } from '../../actions/currentCourse';
+import { COURSE_EDIT } from '../../constants/routes';
 
 export class Courses extends Component {
   constructor(props) {
@@ -10,6 +13,21 @@ export class Courses extends Component {
     this.state = {
       courses: []
     };
+  }
+
+  addCourse = async () => {
+    const newCourse = {name: "", description: ""}
+    const response = await fetch('https://cors-anywhere.herokuapp.com/54.191.130.113:8080/api/admin/v1/courses', {
+      method: 'POST',
+      headers: {
+        "content-type": 'application/json'
+      },
+      body: JSON.stringify(newCourse)
+    })
+    const data = await response.json()
+
+    this.props.updateCurrentCourse(data.data);
+    this.props.history.push(COURSE_EDIT)
   }
 
   componentDidMount() {
@@ -28,6 +46,7 @@ export class Courses extends Component {
 
     return ( 
       <div className='courses_container'>
+        <button onClick={this.addCourse}>Add New Course</button>
         {this.state.courses.length ? 
           courses : 
           <img height='200' width='200' src='http://gifimage.net/wp-content/uploads/2017/09/ajax-loading-gif-transparent-background-2.gif'/>}
@@ -39,3 +58,9 @@ export class Courses extends Component {
 export const mapStateToProps = (state) => ({
   user: state.user
 });
+
+export const mapDispatchToProps = (dispatch) => ({
+  updateCurrentCourse: (currentCourse) => dispatch(updateCurrentCourse(currentCourse))
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Courses))
