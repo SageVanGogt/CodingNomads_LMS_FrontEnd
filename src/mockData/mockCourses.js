24fbcabe451fc467ebb332fe5d649ce3d91721b1
Daniela Carey <danielafcarey@gmail.com>
2018-07-29 14:02:33 -0600

    Update mockCourses to be correct response format

--- src/mockData/mockCourses.js
+++ src/mockData/mockCourses.js
@@ -1,17 +1,19 @@
-export const mockCourses = [
-  {
-    id: 1,
-    name: 'Java',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent viverra nibh lacinia, sodales ipsum id, efficitur quam. Cras et felis tincidunt, ornare erat in, accumsan erat. Nulla facilisi.'
-  },
-  {
-    id: 2,
-    name: 'Javascript',
-    description: 'Lorem ipsum variable semicolon, consectetur adipiscing elit.Praesent viverra nibh lacinia, sodales ipsum id, tincidunt, ornare erat in, accumsan erat.'
-  },
-  {
-    id: 3,
-    name: 'Python',
-    description: 'elit. Praesent viverra nibh lacinia, sodales ipsum id, efficitur quam. Cras et felis tincidunt, ornare erat in, accumsan erat. Nulla facilisi.  '
-  }
-];
+export const mockCourses = {
+  data: [
+    {
+      id: 1,
+      name: 'Java',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent viverra nibh lacinia, sodales ipsum id, efficitur quam. Cras et felis tincidunt, ornare erat in, accumsan erat. Nulla facilisi.'
+    },
+    {
+      id: 2,
+      name: 'Javascript',
+      description: 'Lorem ipsum variable semicolon, consectetur adipiscing elit.Praesent viverra nibh lacinia, sodales ipsum id, tincidunt, ornare erat in, accumsan erat.'
+    },
+    {
+      id: 3,
+      name: 'Python',
+      description: 'elit. Praesent viverra nibh lacinia, sodales ipsum id, efficitur quam. Cras et felis tincidunt, ornare erat in, accumsan erat. Nulla facilisi.  '
+    }
+  ]
+};
