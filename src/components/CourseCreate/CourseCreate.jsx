372e5601b31add642a6188842fe136c497ee33a7
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-29 14:27:46 -0600

    Add patchCourse method

--- src/components/CourseCreate/CourseCreate.jsx
+++ src/components/CourseCreate/CourseCreate.jsx
@@ -66,6 +66,29 @@ export class CourseCreate extends Component {
     }
   }
 
+  patchCourse = async () => {
+    const { name, description, tasks } = this.state;
+    const updatedCourse = {
+      id: this.props.currentCourse.id,
+      name,
+      description,
+      tasks
+    };
+
+    try {
+      const response = await fetch('https://cors-anywhere.herokuapp.com/54.191.130.113:8080/api/admin/v1/students', {
+        method: 'POST',
+        'content-type': 'application/json',
+        body: JSON.stringify(updatedCourse)
+      });
+      if (response.status !== 200) {
+        throw Error('Error.');
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
   componentDidMount() {
     this.fetchTasks();
     this.fetchStudents();
