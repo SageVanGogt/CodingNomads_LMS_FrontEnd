b325f7dfd5f0e2e150f52b28e8f9cd0bb5a3fc39
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-29 15:44:16 -0600

    Add inline styling to input style image, for click functionality. Will be delete button

--- src/components/CourseCreate/CourseCreate.jsx
+++ src/components/CourseCreate/CourseCreate.jsx
@@ -57,6 +57,10 @@ export class CourseCreate extends Component {
     }
   }
 
+  deleteTask = (id) => {
+    console.log(id)
+  }
+
   handleStudentSelect = (e) => {
     const student = this.state.allStudents.find(student => student.name === e.target.value);
     if (!this.state.students.includes(student) && student.name !== "Select Student") {
@@ -75,7 +79,7 @@ export class CourseCreate extends Component {
       description,
       tasks
     };
-    
+
     try {
       const response = await fetch('https://cors-anywhere.herokuapp.com/54.191.130.113:8080/api/admin/v1/courses', {
         method: 'PATCH',
@@ -125,7 +129,6 @@ export class CourseCreate extends Component {
         <form 
           action="submit" 
           className="CourseCreate_form"
-          onSubmit={this.patchCourse}
         >
           <input
             type="text"
@@ -155,7 +158,7 @@ export class CourseCreate extends Component {
           >
             {students}
           </select>
-          <button type="submit">Submit Course</button>
+          <button type="submit" onSubmit={(e) => this.patchCourse(e)}>Submit Course</button>
         </form>
       </div>
     );
--- src/components/CourseTaskCard/CourseTaskCard.jsx
+++ src/components/CourseTaskCard/CourseTaskCard.jsx
@@ -4,9 +4,20 @@ import './CourseTaskCard.css';
 export const CourseTaskCard = (props) => {
   const {id, name, description} = props;
 
+  const inputStyle = {
+    'width': 10 + 'px',
+    'float': 'right',
+    'padding-right': 3 + 'px',
+    'padding-top': 3 + 'px',
+    'cursor': 'initial',
+    'height': 10 + 'px',
+    'margin': 0
+  }
+  
   return (
     <div className='CourseTaskCard' draggable onDragStart={(e) => console.log(e)}>
-      <h4 title={description}>{name}</h4>
+      <h4 title={description} display='inline-block'>{name}</h4>
+      <input type='image' src="/button.png" style={inputStyle} onClick={() => console.log('hello')}/>
     </div>
   );
 };
