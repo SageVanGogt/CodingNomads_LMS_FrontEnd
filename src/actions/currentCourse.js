ae15471a04305a3bb4275632968b395d1d8fff16
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-30 12:41:22 -0600

    Add REMOVE_CURRENT_COURSE action and reducer functionality

--- src/actions/currentCourse.js
+++ src/actions/currentCourse.js
@@ -2,3 +2,7 @@ export const updateCurrentCourse = course => ({
   type: 'UPDATE_CURRENT_COURSE',
   currentCourse: course
 });
+
+export const removeCurrentCourse = () => ({
+  type: 'REMOVE_CURRENT_COURSE'
+});
--- src/reducers/currentCourseReducer.js
+++ src/reducers/currentCourseReducer.js
@@ -2,6 +2,8 @@ export const currentCourseReducer = (state = {}, action) => {
   switch (action.type) {
     case 'UPDATE_CURRENT_COURSE':
       return action.currentCourse;
+    case 'REMOVE_CURRENT_COURSE':
+      return {}
     default:
       return state;
   }
