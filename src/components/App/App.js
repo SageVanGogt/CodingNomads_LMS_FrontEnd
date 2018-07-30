80444262400ae78c096131375e16a7b084e4d593
Daniela Carey <danielafcarey@gmail.com>
2018-07-30 17:14:03 -0600

    Rearrange nav links and add Link to logo img

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Link,
+  withRouter
+} from 'react-router-dom';
 import * as routes from '../../constants/routes';
 import LogIn from '../LogIn/LogIn';
 import AdminHome from '../AdminHome/AdminHome';
@@ -16,13 +21,18 @@ class App extends Component {
       <Router>
         <div className="App">
           <nav className="nav__site">
-           <img className="logo" alt="Coding Nomads" src="/logo.png" />
+            <Link to={routes.HOME}>
+              <img 
+                className="logo" 
+                alt="Coding Nomads" 
+                src="/logo.png" 
+              />
+            </Link>
             <ul aria-label="main navigation list">
-              <li><Link to={routes.TASKS}> Profile</Link></li>
-              <li><Link to={routes.STUDENTS_EDIT}> Students</Link></li>
               <li><Link to={routes.COURSES}> Courses</Link></li>
               <li><Link to={routes.TASKS}> Tasks</Link></li>
-              <li><Link to={routes.TASKS}> Calendar</Link></li>
+              <li><Link to={routes.STUDENTS_EDIT}> Students</Link></li>
+              <li><Link to={routes.TASKS}> Profile</Link></li>
             </ul>
           </nav>
           <Route
@@ -64,4 +74,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
