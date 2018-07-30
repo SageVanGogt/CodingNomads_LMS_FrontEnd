84e43f27e0f0ff4f5d7007acbf31640b40bd07da
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-30 14:53:41 -0600

    Adjust COURSE_EDIT route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -5,6 +5,6 @@ export const COURSES = '/courses';
 export const TASKS = '/tasks';
 export const TASKS_EDIT = '/tasks/edit';
 export const TASK_VIEW = '/tasks';
-export const COURSE_EDIT = '/edit-course';
+export const COURSE_EDIT = '/course/edit';
 export const STUDENTS_EDIT = '/students/edit';
 export const STUDENTS = '/students';
