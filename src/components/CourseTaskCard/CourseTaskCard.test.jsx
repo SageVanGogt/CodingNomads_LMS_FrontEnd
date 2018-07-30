993b7946fcc2b30f22fea079c01d5529f219e545
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-30 13:54:20 -0600

    Add CourseTaskCard snapshot test
    Simple component, simple test

--- src/components/CourseTaskCard/CourseTaskCard.test.jsx
+++ src/components/CourseTaskCard/CourseTaskCard.test.jsx
@@ -4,6 +4,8 @@ import { CourseTaskCard } from './CourseTaskCard';
 
 describe('CourseTaskCard', () => {
   it('matches the snapshot', () => {
+    const wrapper = shallow(<CourseTaskCard />)
 
-  })
-})
+    expect(wrapper).toMatchSnapshot();
+  });
+});
