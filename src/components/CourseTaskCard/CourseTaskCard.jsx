523b528c0750064d4dc844a4d5adb62856e2c863
Daniela Carey <danielafcarey@gmail.com>
2018-07-31 19:18:02 -0600

    Add alt to img tag in CourseTaskCard

--- src/components/CourseTaskCard/CourseTaskCard.jsx
+++ src/components/CourseTaskCard/CourseTaskCard.jsx
@@ -5,9 +5,19 @@ export const CourseTaskCard = (props) => {
   const {id, name, description} = props;
 
   return (
-    <div className='CourseTaskCard' draggable onDragStart={(e) => console.log(e)}>
-      <h4 title={description} display='inline-block'>{name}</h4>
-      <img src="/button.png" onClick={() => props.deleteTask(id)}/>
+    <div 
+      className='CourseTaskCard' 
+      draggable 
+      onDragStart={(e) => console.log(e)} >
+      <h4 
+        title={description} 
+        display='inline-block'>{name}
+      </h4>
+      <img 
+        src="/button.png" 
+        alt="delete task"
+        onClick={() => props.deleteTask(id)}
+      />
     </div>
   );
 };
