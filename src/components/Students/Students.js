23b8683ee44bfd0face72de6e752577f40a5df4e
Daniela Carey <danielafcarey@gmail.com>
2018-07-31 19:21:23 -0600

    Remove unused variables and constructor in Students

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './Students.css';
-import * as API from '../../apiCalls/apiCalls';
-import PropTypes from 'prop-types';
 
 export class Students extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
     return (
       <div className="Students_page">
